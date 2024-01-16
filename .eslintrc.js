0c58763b07adf8dccef8264699be5dcd10438469
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-01-15 19:46:40 -0800

    fix: removed eslintrc.js 'next' plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,40 +1,42 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'standard-with-typescript',
-    'plugin:react/recommended',
-    'eslint:recommended',
-    'plugin:react-hooks/recommended',
-    'prettier',
-  ],
-  overrides: [
-    {
-      files: ['*.js', '*.cjs'],
-      excludedFiles: '*.ts, *.tsx',
-      rules: {
-        '@typescript-eslint/*': 'off',
-      },
-      parserOptions: {
-        sourceType: 'script',
-      },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
     },
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-  },
-  plugins: ['react', 'prettier'],
-  rules: {
-    'react/react-in-jsx-scope': 'off', // Safely disable this rule as Next.js has React built-in
-    'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
-    'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
-    'promise/param-names': 'off',
-    '@typescript-eslint/explicit-function-return-type': 'warn',
-    '@typescript-eslint/no-misused-promises': 'off',
-    'prettier/prettier': 'error',
-  },
+    extends: [
+        'next',
+        'next/core-web-vitals',
+        'standard-with-typescript',
+        'plugin:react/recommended',
+        'eslint:recommended',
+        'plugin:react-hooks/recommended',
+        'prettier',
+    ],
+    overrides: [
+        {
+            files: ['*.js', '*.cjs'],
+            excludedFiles: '*.ts, *.tsx',
+            rules: {
+                '@typescript-eslint/*': 'off',
+            },
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    plugins: ['react', 'prettier'],
+    rules: {
+        'react/react-in-jsx-scope': 'off', // Safely disable this rule as Next.js has React built-in
+        'react-hooks/rules-of-hooks': 'error', // Checks rules of Hooks
+        'react-hooks/exhaustive-deps': 'warn', // Checks effect dependencies
+        'promise/param-names': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'warn',
+        '@typescript-eslint/no-misused-promises': 'off',
+        'prettier/prettier': 'error',
+    },
 };
