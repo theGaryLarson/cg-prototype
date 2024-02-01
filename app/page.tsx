67cdea24b50168139b51e60a00b320d0c1b52cb4
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-01-31 19:55:16 -0800

    sanity check:  used prisma to return users and display   in User component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,25 @@
 /* Components */
 import { Counter } from './components/Counter/Counter';
 
-export default function IndexPage() {
-  return <Counter />;
+import prisma from '../lib/prisma';
+import User from './components/PrismaExample/User';
+
+import React from 'react';
+
+export default async function IndexPage(): Promise<React.ReactElement<any, string>> {
+  const feed = await prisma.user.findMany();
+  return (
+    <div>
+      <Counter />
+      <>
+        {feed.map((user) => (
+          <div key={user.userId}>
+            <User user={user} />
+          </div>
+        ))}
+      </>
+    </div>
+  );
 }
 
 export const metadata = {
