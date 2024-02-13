2e44e96c1c23405644fcadf4744d693e3c8ae12d
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-02-12 23:46:09 -0800

    Enable 'prettier' in ESLint configuration
    The 'prettier' rule in the ESLint configuration file (`/.eslintrc.cjs`) was previously commented out and has been enabled to improve code formatting.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,7 +11,7 @@ module.exports = {
         // 'plugin:react/recommended',
         // 'eslint:recommended',
         // 'plugin:react-hooks/recommended',
-        // 'prettier',
+        'prettier',
     ],
     overrides: [
         {
