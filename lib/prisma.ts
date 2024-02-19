e67c08daa1ee2c11bf89b6ba9a8f4849ff66206d
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-02-19 12:21:56 -0800

    Remove unnecessary eslint directive in prisma.ts
    After adjusting the eslintrc file this rule is no longer defined.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,7 +2,6 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-// eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
 export const prisma = globalForPrisma.prisma ? globalForPrisma.prisma : new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
