a5303d9141fcbddd93a104f02d8712790b924849
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-03-08 09:39:26 -0800

    Add job listings with skills to IndexPage
    Proof of concept. Best to call prisma directly in SSC for optimization.

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,66 @@
 /* Components */
 import { Counter } from './components/Counter/Counter';
-
+import JobSkillsCompany from './components/PrismaExample/JobSkillsCompany';
 import prisma from '../lib/prisma';
+import { User as UserData } from '@prisma/client';
 import User from './components/PrismaExample/User';
-
-import React from 'react';
+import ModalTrigger from '@/app/components/ModalTrigger';
 
 export default async function IndexPage(): Promise<React.ReactElement<any, string>> {
-  const feed = await prisma.user.findMany();
+  const feed: UserData[] = await prisma.user.findMany();
+
+  const jobListingsWithSkills = await prisma.jobListing.findMany({
+    select: {
+      job_listing_id: true,
+      job_title: true,
+      position_loc: true,
+      salary_range: true,
+      region: true,
+      employer: {
+        select: {
+          title: true,
+        },
+      },
+      jobListingSkillCategories: {
+        select: {
+          skill_category: {
+            select: {
+              category_name: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
   return (
-    <div>
+    <>
       <Counter />
-      <>
-        {feed.map((user) => (
-          <div key={user.userId}>
-            <User user={user} />
-          </div>
-        ))}
-      </>
-    </div>
+      <div className="grid grid-cols-1 gap-10 prose w-full">
+        {' '}
+        {/**/}
+        <div>
+          <h1>Users</h1>
+          {feed.map((user) => (
+            <div key={user.userId}>
+              <User user={user} />
+            </div>
+          ))}
+        </div>
+        <div>
+          <h1>Job Listings With Skills</h1>
+          {jobListingsWithSkills.map((listing: JobListingWithSkillsAndCompany) => (
+            <div key={listing.job_listing_id}>
+              <pre className="p-0">{JSON.stringify(listing, null, 2)}</pre>
+              <JobSkillsCompany listing={listing} />
+            </div>
+          ))}
+        </div>
+        <div>
+          <h2>Next Test</h2>
+        </div>
+      </div>
+    </>
   );
 }
 
